98312a70f3b4c4c7bdd9c4a4a993297e6b12b77b
Dmitry Kuznetsov <d.j.kuznetsov@yandex.ru>
2023-04-05 21:37:25 +0300

    technical specification #11 code style correction #14

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.EmailLoginAlreadyUsed;
 import ru.yandex.practicum.filmorate.exception.ResourceNotFoundException;
