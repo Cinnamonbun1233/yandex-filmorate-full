f14d4ef95c9e229929591dc5184d061db5cf6d21
Dmitry Kuznetsov <d.j.kuznetsov@yandex.ru>
2023-04-05 21:42:49 +0300

    technical specification #11 code style correction #15

--- src/test/java/ru/yandex/practicum/filmorate/mock_mvc/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/mock_mvc/FilmControllerTest.java
@@ -7,13 +7,11 @@ import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
-import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 
 import java.util.List;
 
