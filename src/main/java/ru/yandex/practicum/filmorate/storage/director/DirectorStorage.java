74d4c4586a1fc706178c8a1e3a383657570913d6
Dmitry Kuznetsov <d.j.kuznetsov@yandex.ru>
2023-04-26 15:21:22 +0300

    feature: add director code style correction #2

--- src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorStorage.java
@@ -6,9 +6,9 @@ import java.util.List;
 import java.util.Set;
 
 public interface DirectorStorage {
-    
+
     Director addDirector(Director director);
-    
+
     boolean hasTwin(Director director);
 
     Director getDirector(Long id);
