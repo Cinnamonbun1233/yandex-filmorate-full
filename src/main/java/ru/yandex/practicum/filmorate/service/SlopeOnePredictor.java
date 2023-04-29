a1369f2ab1df120c2a2d6917a168d7b02caa3db9
Dmitry Kuznetsov <d.j.kuznetsov@yandex.ru>
2023-04-29 20:42:32 +0300

    feature: add-recommendations. code style correction #9

--- src/main/java/ru/yandex/practicum/filmorate/service/SlopeOnePredictor.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/SlopeOnePredictor.java
@@ -8,11 +8,11 @@ import java.util.stream.Collectors;
 
 public class SlopeOnePredictor {
 
-	private Map<Long, HashMap<Long, Double>> inputData; // user_id, item_id
-	private List<Long> items; // item_id
-	private Map<Long, Map<Long, Double>> diff = new HashMap<>(); // item_id, item_id
-	private Map<Long, Map<Long, Integer>> freq = new HashMap<>(); // item_id, item_id
-	private Map<Long, HashMap<Long, Double>> outputData = new HashMap<>(); // user_id, item_id
+	private final Map<Long, HashMap<Long, Double>> inputData; // user_id, item_id
+	private final List<Long> items; // item_id
+	private final Map<Long, Map<Long, Double>> diff = new HashMap<>(); // item_id, item_id
+	private final Map<Long, Map<Long, Integer>> freq = new HashMap<>(); // item_id, item_id
+	private final Map<Long, HashMap<Long, Double>> outputData = new HashMap<>(); // user_id, item_id
 
 	public SlopeOnePredictor(Map<Long, HashMap<Long, Double>> inputData) {
 
@@ -94,6 +94,7 @@ public class SlopeOnePredictor {
 						uPred.put(k, uPred.get(k) + finalValue);
 						uFreq.put(k, uFreq.get(k) + freq.get(k).get(j).intValue());
 					} catch (NullPointerException e1) {
+						double predictedValue = -1;
 					}
 				}
 			}
